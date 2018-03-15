cfb44087bd1df5a83f0d1abdff1a7433d6558ea3
Dennis Wu <dennissfwu@gmail.com>
2018-03-15 16:01:13 -0700

    Use second database connection for Shipworks

--- app/models/shipworks/application_record.rb
+++ app/models/shipworks/application_record.rb
@@ -1,6 +1,7 @@
 module Shipworks
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
+    establish_connection SHIPWORKS_DB if defined? SHIPWORKS_DB
 
     def self.table_name
       self.name.split('::').last

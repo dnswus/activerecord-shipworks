46d9adc7bf36529eeafea1c9a99fcb5f6f050861
Dennis Wu <dennissfwu@gmail.com>
2018-03-15 19:04:21 -0700

    Read in SHIPWORKS_DATABASE_URL environment variable for Shipworks database connection

--- test/dummy/config/initializers/shipworks_db.rb
+++ test/dummy/config/initializers/shipworks_db.rb
@@ -1,1 +1,1 @@
-SHIPWORKS_DB = YAML.load(ERB.new(IO.read(File.join(Rails.root, "config", "shipworks_database.yml"))).result)[Rails.env.to_s]
+SHIPWORKS_DB = ENV['SHIPWORKS_DATABASE_URL'] || YAML.load(ERB.new(IO.read(File.join(Rails.root, "config", "shipworks_database.yml"))).result)[Rails.env.to_s]
